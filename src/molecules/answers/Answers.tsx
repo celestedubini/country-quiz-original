582a56a3cb16868bd4d3bf26e482150713a13657
celestedubini <celeste.dubini@mediamonks.com>
2022-07-05 20:43:20 -0300

    Revert "Using local state for selected answer index"
    This reverts commit 3e9a6c9f97c5543497721329879e0144dadc0b83.

--- src/molecules/answers/Answers.tsx
+++ src/molecules/answers/Answers.tsx
@@ -18,7 +18,7 @@ function Answers({options, alreadyAnswered, rightCountry, settingAnswer, setCorr
     const ultimateHandle = (event: React.MouseEvent) => {
         if (!alreadyAnswered) {
             setClicked(parseInt(event.currentTarget.getAttribute("value")!));
-            if (rightCountry === clicked) {
+            if (rightCountry === parseInt(event.currentTarget.getAttribute("value")!)) {
                 setCorrectOrIncorrect ? setCorrectOrIncorrect() : null;
             } else {
                 event.currentTarget.classList.add(styles.incorrect)
