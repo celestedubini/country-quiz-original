824121ccfc956f35cd95d660fcac21bd96c6b05e
celestedubini <celeste.dubini@mediamonks.com>
2022-07-05 20:29:44 -0300

    Added error check on Questions component

--- src/organisms/questions/Questions.tsx
+++ src/organisms/questions/Questions.tsx
@@ -47,11 +47,11 @@ function Questions({didYouLose, addingRightAnswers}: QuestionsProps) {
     return (
         <div className={styles.questionsComponent}>
             {countries.length === 0 && !error && <div className={styles.loader}></div>}
-            {countries.length > 0 && <div className={styles.imageWorld}></div>}
-            {capitalQuestion && countries.length > 0 &&
+            {countries.length > 0 && !error && <div className={styles.imageWorld}></div>}
+            {capitalQuestion && countries.length > 0 && !error &&
                 <CapitalQuestion changeQuestion={changeQuestion} gameOver={didYouLose}
                                  addingRightAnswers={addingRightAnswers} countries={countries}/>}
-            {!capitalQuestion && countries.length > 0 &&
+            {!capitalQuestion && countries.length > 0 && !error &&
                 <FlagQuestion changeQuestion={changeQuestion} gameOver={didYouLose}
                               addingRightAnswers={addingRightAnswers} countries={countries}/>}
             {error && <div><Error/></div>}
